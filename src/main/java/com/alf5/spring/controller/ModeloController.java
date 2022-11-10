a4ebc2e1ea606209516d2bda4132b5d237a6a3be
Luilio Mota <luiliosm@yahoo.com.br>
2022-11-10 09:32:54 -0300

    Primeiro Commit

---
+++ src/main/java/com/alf5/spring/controller/ModeloController.java
@@ -0,0 +1,16 @@
+package com.alf5.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/modelo")
+public class ModeloController {
+
+    @GetMapping
+    public void modelo (){
+        System.out.println("Esta é uma mensagem de teste");
+    }
+}
+
